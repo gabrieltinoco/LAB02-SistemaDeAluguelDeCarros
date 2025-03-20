fbe2470b5bdc25f1dcba1794069efa54e9de4e48
Gabriel L Tinoco <145233378+gabrieltinoco@users.noreply.github.com>
2025-03-20 18:48:58 -0300

    Spring MVC configuração

---
+++ Código/rentwheels/src/main/java/com/lab02/rentwheels/RentwheelsApplication.java
@@ -0,0 +1,13 @@
+package com.lab02.rentwheels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentwheelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentwheelsApplication.class, args);
+	}
+
+}
---
+++ Código/rentwheels/src/test/java/com/lab02/rentwheels/RentwheelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab02.rentwheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentwheelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
