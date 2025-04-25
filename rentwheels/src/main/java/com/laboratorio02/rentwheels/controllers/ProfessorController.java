9fc06806145c2bc30cfd72c40ee8635cbe7b4385
Gabriel L Tinoco <gabriel.taguiar@gmail.com>
2025-04-25 15:46:26 -0300

    Banco Conectado

--- rentwheels/src/main/java/com/laboratorio02/rentwheels/controllers/ProfessorController.java
+++ rentwheels/src/main/java/com/laboratorio02/rentwheels/controllers/ProfessorController.java
@@ -2,6 +2,8 @@ package com.laboratorio02.rentwheels.controllers;
 
 import com.laboratorio02.rentwheels.models.Professor;
 import com.laboratorio02.rentwheels.models.StatusProfessor;
+import com.laboratorio02.rentwheels.repositories.ProfessorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,15 +16,20 @@ import java.util.List;
 @Controller
 public class ProfessorController {
 
+    @Autowired
+    private ProfessorRepository professorRepository;
+
     @GetMapping("/professores")
     public ModelAndView index(){
-        Professor batman = new Professor("Batman", new BigDecimal(5000.0), StatusProfessor.ATIVO);
-        batman.setId(1L);
-        Professor coringa = new Professor("Coringa", new BigDecimal(10000.0), StatusProfessor.APOSENTADO);
-        coringa.setId(2L);
-        Professor mulherMaravilha = new Professor("Mulher Maravilha", new BigDecimal(15000.0), StatusProfessor.INATIVO);
-        mulherMaravilha.setId(3L);
-        List<Professor> professores = Arrays.asList(batman, coringa, mulherMaravilha);
+//        Professor batman = new Professor("Batman", new BigDecimal(5000.0), StatusProfessor.ATIVO);
+//        batman.setId(1L);
+//        Professor coringa = new Professor("Coringa", new BigDecimal(10000.0), StatusProfessor.APOSENTADO);
+//        coringa.setId(2L);
+//        Professor mulherMaravilha = new Professor("Mulher Maravilha", new BigDecimal(15000.0), StatusProfessor.INATIVO);
+//        mulherMaravilha.setId(3L);
+//        List<Professor> professores = Arrays.asList(batman, coringa, mulherMaravilha);
+
+        List<Professor> professores = this.professorRepository.findAll();
 
         ModelAndView mv = new ModelAndView("professores/index");
         mv.addObject("professores", professores);
