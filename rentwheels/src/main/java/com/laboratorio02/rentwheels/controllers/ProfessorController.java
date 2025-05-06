11ecbf15e2fce801ebf63d18fcec7cb95890a3d5
Gabriel L Tinoco <gabriel.taguiar@gmail.com>
2025-05-06 16:19:35 -0300

    v18

--- rentwheels/src/main/java/com/laboratorio02/rentwheels/controllers/ProfessorController.java
+++ rentwheels/src/main/java/com/laboratorio02/rentwheels/controllers/ProfessorController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,6 +17,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ProfessorController {
@@ -52,7 +54,24 @@ public class ProfessorController {
         } else {
             Professor professor = requisicao.toProfessor();
             this.professorRepository.save(professor);
+            return new ModelAndView("redirect:/professores/" + professor.getId());
+        }
+    }
+
+    @GetMapping("/professores/{id}")
+    public ModelAndView show(@PathVariable Long id) {
+
+        Optional<Professor> optional = this.professorRepository.findById(id);
+
+        if (optional.isPresent()) {
+            Professor p = optional.get();
+            ModelAndView mv = new ModelAndView("professores/show");
+            mv.addObject("page", "professores/show");
+            mv.addObject("professor", p);
+            return mv;
+        } else {
             return new ModelAndView("redirect:/professores");
         }
     }
+
 }
